d1e936940c9bfcbc6329563fe6cc57ecbc0a4bd5
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-10-09 11:28:57 +0200

    imp generic Repo- Interface and Class

---
+++ RepositoryMongo_Generic/Class1.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RepositoryMongo_Generic
+{
+    public interface IMongoDbSettings
+    {
+        string DatabaseName { get; set; }
+        string ConnectionString { get; set; }
+    }
+
+    public class MongoDbSettings : IMongoDbSettings
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+    public interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+    }
+
+    public abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
