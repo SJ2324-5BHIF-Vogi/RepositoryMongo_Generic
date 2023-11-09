c5fd74aa37e22940504551bf4cdb630192cde6c5
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-11-09 15:23:38 +0100

    con string

--- RepositoryMongo_Generic.Tests/UnitTest1.cs
+++ RepositoryMongo_Generic.Tests/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace RepositoryMongo_Generic.Tests
         public void Test_for_How_To_Use()
         {
             MongoDbSettings mongoDbSettings = new MongoDbSettings();
+                                                         //user:pw@host
             mongoDbSettings.ConnectionString = "mongodb://root:1234@localhost:27017";
             mongoDbSettings.DatabaseName = "mongodb";
 
